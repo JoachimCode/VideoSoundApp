a0612ec3dd5a6a45d09e876623a326ecc6005296
Joachim Duong <joachim.duong@mobai.bio>
2024-09-18 12:10:04 +0200

    added title input

--- VideoSoundApp/CameraViewController.swift
+++ VideoSoundApp/CameraViewController.swift
@@ -8,19 +8,48 @@ class CameraViewController: UIViewController {
     var captureSession: AVCaptureSession!
     var videoOutput: AVCaptureMovieFileOutput!
     var previewLayer: AVCaptureVideoPreviewLayer!
+    var isRecording: Bool = false
+    var recordingTimer: Timer?
     
-
+    @IBOutlet weak var titleTextField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCamera()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+                view.addGestureRecognizer(tapGesture)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+                NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+    
+    deinit {
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
+    
+    @objc func dismissKeyboard() {
+            view.endEditing(true)
+        }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+            if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+                let keyboardHeight = keyboardFrame.height
+                self.view.frame.origin.y = -keyboardHeight   // Move view up by half of keyboard height
+            }
+        }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+            self.view.frame.origin.y = 0  // Reset view position
+        }
     
     func setupCamera() {
         captureSession = AVCaptureSession()
         captureSession.sessionPreset = .high
         
-        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
             print("No camera available")
             return
         }
@@ -44,13 +73,16 @@ class CameraViewController: UIViewController {
         previewLayer.videoGravity = .resizeAspectFill
         previewLayer.frame = previewView.bounds
         previewView.layer.addSublayer(previewLayer)
-
         captureSession.startRunning()
     }
     
     @IBAction func recordButtonPressed(_ sender: UIButton) {
-        let outputPath = NSTemporaryDirectory() + "output.mov"
-        let outputFileURL = URL(fileURLWithPath: outputPath)
+        guard let outputFileURL = getOutputFileURL() else {
+                    // If there's no valid filename, print an error or show an alert
+                    print("Please enter a valid filename before recording.")
+                    showAlert(message: "Please enter a valid filename before recording.")
+                    return
+                }
 
         if videoOutput.isRecording {
             videoOutput.stopRecording()
@@ -60,7 +92,34 @@ class CameraViewController: UIViewController {
             sender.setTitle("Stop", for: .normal)
         }
     }
+    
+    func getOutputFileURL() -> Optional<URL> {
+            let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+
+            // Check if the user entered a title in the text field
+            let title = titleTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            // If no title is provided, use the current date and time as the file name
+            let fileName: String
+            if let title = title, !title.isEmpty {
+                fileName = title  // Use the user-provided title
+            } else {
+                return nil
+            }
+
+            // Create the full URL for the output file with .mov extension
+            let outputFileURL = documentsDirectory.appendingPathComponent("\(fileName).mov")
+            return outputFileURL
+        }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+
 }
+
 
 extension CameraViewController: AVCaptureFileOutputRecordingDelegate {
     func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
